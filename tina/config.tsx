1b0de0189be38958c94abcf3d36c22e2eed145fa
Kelly Davis <kldavis4@users.noreply.github.com>
2025-02-23 14:47:10 -0500

    Update config.tsx

--- tina/config.tsx
+++ tina/config.tsx
@@ -9,7 +9,7 @@ import { getSession, signIn, signOut } from "next-auth/react";
 
 const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true"
 const config = defineConfig({
-  contentApiUrlOverride: "/api/gql",
+  contentApiUrlOverride: "https://tina-self-hosted-demo-27jfyp73y-kelly-davis-projects.vercel.app/api/gql",
   admin: {
     auth: {
       useLocalAuth: isLocal,
